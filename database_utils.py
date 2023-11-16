7c8b021e03b0cee3597450c55873843d1c956427
Egle-007 <eglei.rasyk@gmail.com>
2023-11-16 20:18:52 +0000

    Adds methods to the DatabaseConnector class

--- database_utils.py
+++ database_utils.py
@@ -1,14 +1,29 @@
 import yaml
+from sqlalchemy import create_engine, inspect
+import pandas as pd
 
 class DatabaseConnector:
     # Class will be used to connect with and upload data to the database. 
     # Created a method read_db_creds this will read the credentials yaml file and return a dictionary of the credentials.
-    def read_db_creds(self):
+    def __read_db_creds__(self):
 
         with open('db_creds.yaml', 'r') as f:
             db_creds = yaml.safe_load(f)
-        print(db_creds)
+        return db_creds
+    
+    def init_db_engine(self, db_creds):
+    #     # Now create a method init_db_engine which will read the credentials from the return of read_db_creds and initialise 
+    #     # and return an sqlalchemy database engine.
+         # crds stands for credentials which will be used for connection to RDS. 
+        engine = create_engine(f"postgresql+psycopg2://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
+        return engine
 
-connector = DatabaseConnector()
-connector.read_db_creds()
+    def list_db_tables(self, engine):
+        inspector = inspect(engine)
+        return inspector.get_table_names()
+    
+
+
+
+
 
