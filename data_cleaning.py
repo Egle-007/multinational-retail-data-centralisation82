c04462b3675102ccbc6a2a9915a67aa3e7b2a8c5
Egle-007 <eglei.rasyk@gmail.com>
2023-11-16 22:01:03 +0000

    Updates classes

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,2 +1,19 @@
+from database_utils import DatabaseConnector
+from data_extraction import DataExtractor
+import pandas as pd
+
 class DataCleaning:
+    def __init__(self):
+        self.connector = DatabaseConnector()
+        self.extractor = DataExtractor()
     # with methods to clean data from each of the data sources.
+    def clean_user_data(self):
+        read_user_data = self.extractor.read_rds_table(self.connector,'legacy_users')
+        return read_user_data
+        
+
+clean = DataCleaning()
+v = clean.clean_user_data()
+print(v)
+
+        
--- database_utils.py
+++ database_utils.py
@@ -1,17 +1,20 @@
 import yaml
 from sqlalchemy import create_engine, inspect
 import pandas as pd
+import psycopg2
+
 
 class DatabaseConnector:
     # Class will be used to connect with and upload data to the database. 
     # Created a method read_db_creds this will read the credentials yaml file and return a dictionary of the credentials.
-    def __read_db_creds__(self):
-
+    def read_db_creds(self):
+      
         with open('db_creds.yaml', 'r') as f:
             db_creds = yaml.safe_load(f)
         return db_creds
     
-    def init_db_engine(self, db_creds):
+    def init_db_engine(self):
+        db_creds = self.read_db_creds()
     #     # Now create a method init_db_engine which will read the credentials from the return of read_db_creds and initialise 
     #     # and return an sqlalchemy database engine.
         engine = create_engine(f"postgresql+psycopg2://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
@@ -19,8 +22,19 @@ class DatabaseConnector:
 
     def list_db_tables(self, engine):
         inspector = inspect(engine)
-        return inspector.get_table_names()
-    
+        for table in inspector.get_table_names():
+            print(table)
+   
+
+# con = DatabaseConnector()
+# v = con.read_db_creds()
+# h = con.init_db_engine(v)
+# m = con.list_db_tables(h)
+# print(m)
+
+
+# ['legacy_store_details', 'legacy_users', 'orders_table']
+
 
 
 
