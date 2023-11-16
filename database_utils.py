6243a600c7797ca72f9e53db38de8f1a53911334
Egle-007 <eglei.rasyk@gmail.com>
2023-11-16 17:52:00 +0000

    Adds read_db_creds method to the DBConnector class.

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,14 @@
+import yaml
+
 class DatabaseConnector:
-    # which you will use to connect with and upload data to the database.
-    
+    # Class will be used to connect with and upload data to the database. 
+    # Created a method read_db_creds this will read the credentials yaml file and return a dictionary of the credentials.
+    def read_db_creds(self):
+
+        with open('db_creds.yaml', 'r') as f:
+            db_creds = yaml.safe_load(f)
+        print(db_creds)
+
+connector = DatabaseConnector()
+connector.read_db_creds()
+
