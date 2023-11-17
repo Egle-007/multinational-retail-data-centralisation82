cdd14db64ac59d8e99ed703c0b3dfa3175382196
Egle-007 <eglei.rasyk@gmail.com>
2023-11-17 16:21:27 +0000

    Prints .dtypes, .describe, to check info about the table

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,16 +4,22 @@ import pandas as pd
 
 class DataCleaning:
     def __init__(self):
-        self.connector = DatabaseConnector()
-        self.extractor = DataExtractor()
+        self.connector = DatabaseConnector()    # Where are we getting data from
+        self.extractor = DataExtractor()        # Returns df_copy
     # with methods to clean data from each of the data sources.
     def clean_user_data(self):
         read_user_data = self.extractor.read_rds_table(self.connector,'legacy_users')
-        return read_user_data
+        print(read_user_data.dtypes)
+        print(read_user_data.describe())
+        
+
         
 
 clean = DataCleaning()
 v = clean.clean_user_data()
 print(v)
 
-        
+
+# Create a method called clean_user_data in the DataCleaning class which will perform the cleaning of the user data.
+# You will need clean the user data, look out for NULL values, errors with dates, incorrectly typed values and rows 
+# filled with the wrong information.
