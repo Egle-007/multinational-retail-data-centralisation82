6451e739a8958f90e099d0f528c7f976db370f45
Egle-007 <eglei.rasyk@gmail.com>
2023-12-06 15:51:20 +0000

    Uploads config.py.

---
+++ config.py
@@ -0,0 +1,6 @@
+header = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+endpoint_number = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+endpoint_store = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
+
+# This file shouldn't be tracked because it is carying a password (credentials), 
+# however due to the purpose of someone evaluating this project, this file is not included in .gitignore.
