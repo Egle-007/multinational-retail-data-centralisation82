b11d03d5644456918bb9860b0961e704d34c6d07
Egle-007 <eglei.rasyk@gmail.com>
2023-12-06 15:45:35 +0000

    Updates docstrings and comments.

--- database_utils.py
+++ database_utils.py
@@ -1,34 +1,43 @@
 import yaml
 from sqlalchemy import create_engine, inspect
-# import pandas as pd
-# import psycopg2
-# from  data_cleaning import DataCleaning
-# import localdb_creds
 
-class DatabaseConnector:                                                                                    # Class will be used to connect with a database.             
-   
-    def _read_db_creds_(self):                                                                              # read_db_creds method will read the credentials yaml file and return a dictionary of the credentials.
-        with open('db_creds.yaml', 'r') as f:
-            db_creds = yaml.safe_load(f)
+
+class DatabaseConnector:                                                                                   
+    ''' 
+    DatabaseConnector is a class used to connect to remote database (db).
+    -------
+    Methods:
+    -------
+    _read_db_creds_(self)
+        Read the credentials in yaml file and return a dictionary of the credentials.
+    init_db_engine(self)
+        Read the credentials from the return of read_db_creds and initialise (use the credentials to log into the db) and return an sqlalchemy database engine.
+    list_db_tables(self, engine)
+        Print table names.
+    '''
+
+
+    def _read_db_creds_(self):                          # protected method not to be included in the autocompletion hints        
+        with open('db_creds.yaml', 'r') as f:           # takes a .yaml file in reading mode and 
+            db_creds = yaml.safe_load(f)                # uses the safe_load method from the yaml package to read data from a YAML file 
             
         return db_creds
 
     
-    def init_db_engine(self):                                                                               # init_db_engine method will read the credentials from the return of read_db_creds and initialise and return an sqlalchemy database engine.
-        db_creds = self._read_db_creds_()
-        engine = create_engine(f"postgresql+psycopg2://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@
-                               {db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
-        return engine
+    def init_db_engine(self):                           # method to be used for connection to the AWS RDS
+        db_creds = self._read_db_creds_()               # uses previous method to access the credentials for not having to write the credentials directly into the code
+        engine = create_engine(f"postgresql+psycopg2://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
+        return engine                                   
 
-    def list_db_tables(self, engine):                                                                       # list_db_tables will list table names             
-        inspector = inspect(engine)
+    def list_db_tables(self, engine):                                
+        inspector = inspect(engine)                     # uses inspect() function to get access to table names in the db
         for table in inspector.get_table_names():
             print(table)     
 
            
 connector = DatabaseConnector()
 
-# ['legacy_store_details', 'legacy_users', 'orders_table']
+
 
 
 
